504411126addddfb68db4c63cd21f1f89907175a
aliezaheer <alizaheer67@gmail.com>
2022-09-12 15:11:12 +0500

    gallery responsivness

--- src/components/HomeGallery/GalleyArea.js
+++ src/components/HomeGallery/GalleyArea.js
@@ -7,11 +7,11 @@ import fourthImg from "../../assets/gallery4.jpg";
 const GalleyArea = () => {
   return (
     <>
-      <div className="flex justify-center mt-14 space-x-7">
-        <img src={firstImg} alt="" />
-        <img src={secondImg} alt="" />
-        <img src={thirdImg} alt="" />
-        <img src={fourthImg} alt="" />
+      <div className="flex justify-center mr-auto ml-auto mt-14 space-x-5">
+        <img className="w-3/12" src={firstImg} alt="" />
+        <img className="w-3/12" src={secondImg} alt="" />
+        <img className="w-3/12" src={thirdImg} alt="" />
+        <img className="w-3/12" src={fourthImg} alt="" />
       </div>
     </>
   );
